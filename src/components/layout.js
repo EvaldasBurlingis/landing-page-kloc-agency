6df7a526667cec02bca4f75f00ba3c70021d9c4c
Evaldas <evabur@icloud.com>
2019-11-25 10:21:27 +0000

    postcss + tailwindcss config

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
+import "../styles/main.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="w-full min-h-screen bg-gray-200">
       {children}
     </div>
   )
