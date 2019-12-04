152ecd389f0f01fa10bab88a9cc6ab8e38cf7000
Evaldas <evabur@icloud.com>
2019-12-04 10:38:03 +0000

    animation hotfix

--- src/components/sections/clients.js
+++ src/components/sections/clients.js
@@ -1,13 +1,17 @@
 import React from "react"
 import ClientsCard from "../cards/clients"
-import { Reveal, Animation, AnimatedTitle } from "react-genie"
+import { Reveal, Animation, RevealMode } from "react-genie"
 
 const OurClientsSection = () => (
   <section className="w-full pt-16">
     <div className="container mx-auto px-4 pb-32 -mt-10">
-      <AnimatedTitle className="font-bold text-3xl md:text-5xl text-csdark mb-6 text-left lg:text-center">
-        Some of our clients
-      </AnimatedTitle>
+      <Reveal animation={Animation.FadeInUp} mode={RevealMode.Clone}>
+        <div>
+          <h2 className="font-bold text-3xl md:text-5xl text-csdark mb-6 text-left lg:text-center">
+            Some of our clients
+          </h2>
+        </div>
+      </Reveal>
       <Reveal animation={Animation.FadeInUp}>
         <ClientsCard />
       </Reveal>
--- src/components/sections/services.js
+++ src/components/sections/services.js
@@ -2,15 +2,19 @@ import React from "react"
 import ServicesImage from "../svg/services"
 import ServicesCard from "../cards/services"
 import AccordionMenu from "../accordion"
-import { Reveal, Animation, AnimatedTitle} from "react-genie"
+import { Reveal, Animation , RevealMode} from "react-genie"
 import { servicesContent } from "../../content/services"
 
 const ServicesSection = () => (
   <section className="w-full pt-16">
     <div className="container mx-auto px-4">
-      <AnimatedTitle className="font-bold text-3xl md:text-5xl text-csdark mb-6 text-left lg:text-center">
-        How can we help you?
-      </AnimatedTitle>
+      <Reveal animation={Animation.FadeInUp} mode={RevealMode.Clone}>
+        <div>
+          <h2 className="font-bold text-3xl md:text-5xl text-csdark mb-6 text-left lg:text-center">
+            How can we help you?
+          </h2>
+        </div>
+      </Reveal>
       <div className="w-full flex flex-wrap flex-col lg:flex-row lg:pt-10">
         <div className="w-full flex items-center justify-center order-2 md:mb-8 lg:w-3/5 lg:order-1 hidden xl:inline-block">
           <Reveal animation={Animation.FadeInUp}>
