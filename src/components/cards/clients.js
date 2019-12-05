e1b707da987ab5f50e1958636fac8498904097d9
Evaldas <evabur@icloud.com>
2019-12-05 11:08:35 +0000

    refactored logos

--- src/components/cards/clients.js
+++ src/components/cards/clients.js
@@ -6,6 +6,45 @@ import ApdoLogo from "../../images/apdologo.png"
 import TravelNurseLogo from "../../images/travelnurse.png"
 import StewartsLogo from "../../images/stewartslogo.jpg"
 
+const clientLogos = [
+  {
+    image: FitbitLogo,
+    alt: "Fitbit Logo",
+    container: "border-r border-gray-200 border-b",
+    imageClass: "w-40 h-auto"
+  },
+  {
+    image: AboveTheLineLogo,
+    alt: "Above The Line logo",
+    container: "border-r border-gray-200 border-b",
+    imageClass: "w-40 h-auto"
+  },
+  {
+    image: TheContractChairLogo,
+    alt: "The Contract Chair logo",
+    container: "border-gray-200 border-b border-r md:border-r-0",
+    imageClass: "w-40 h-auto"
+  },
+  {
+    image: ApdoLogo,
+    alt: "Apdo logo",
+    container: "border-r border-gray-200 border-b md:border-b-0",
+    imageClass: "w-20 h-auto"
+  },
+  {
+    image: TravelNurseLogo,
+    alt: "Travelnurse Logo",
+    container: "border-r border-gray-200",
+    imageClass: "w-32 h-auto"
+  },
+  {
+    image: StewartsLogo,
+    alt: "Stewarts Coaches Logo",
+    container: "",
+    imageClass: "w-40 h-auto"
+  },
+]
+
 const classes = {
   image: {
     container: "w-1/2 md:w-1/3 h-32 flex justify-center items-center hover:shadow-xl"
@@ -14,36 +53,11 @@ const classes = {
 
 const ClientsCard = () => (
   <div className="bg-white flex flex-wrap rounded shadow-xl relative z-10">
-    <div className={`${classes.image.container}  border-r border-gray-200 border-b`}>
-      <img src={FitbitLogo} className="w-40 h-auto" alt="Fitbit logo" />
-    </div>
-    <div className={`${classes.image.container}  border-r border-gray-200 border-b`}>
-      <img
-        src={AboveTheLineLogo}
-        className="w-40 h-auto"
-        alt="Above The Line logo"
-      />
-    </div>
-    <div className={`${classes.image.container}  border-gray-200 border-b border-r md:border-r-0`}>
-      <img
-        src={TheContractChairLogo}
-        className="w-40 h-auto"
-        alt="The Contract Chair logo"
-      />
-    </div>
-    <div className={`${classes.image.container}  border-r border-gray-200 border-b md:border-b-0`}>
-      <img src={ApdoLogo} className="w-20 h-auto" alt="Apdo logo" />
-    </div>
-    <div className={`${classes.image.container}  border-r border-gray-200`}>
-      <img
-        src={TravelNurseLogo}
-        className="w-32 h-auto"
-        alt="Travelnurse Logo"
-      />
-    </div>
-    <div className={`${classes.image.container} `}>
-      <img src={StewartsLogo} className="w-40 h-auto" alt="Stewarts Coaches Logo" />
-    </div>
+    {clientLogos.map(clientLogo => (
+      <div className={`${classes.image.container} ${clientLogo.container}`}>
+        <img src={clientLogo.image} className={clientLogo.imageClass} alt={clientLogo.alt} />
+      </div>
+    ))}
   </div>
 )
 
